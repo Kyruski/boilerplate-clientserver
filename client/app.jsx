aa5250d8838bb49a095ae6b03631bf3e65e9b0d3
kyruski <jamescboyett@gmail.com>
2019-03-11 11:45:16 -0500

    Inititual Commit

---
+++ client/app.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const App = function(props) {
+  return (
+    <div>
+      Welcome
+      <div />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './client/app.jsx',
+  output: {
+    filename: 'index.js',
+    path: `${__dirname}/public`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
